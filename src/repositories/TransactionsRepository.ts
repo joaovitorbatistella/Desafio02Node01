0ecb9317cdc46bee821c849456e8db552c07f18f
João Vítor Batistella <joaovitor.batistella.ifrs@gmail.com>
2020-04-28 22:55:05 -0300

    first commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,21 @@
 import Transaction from '../models/Transaction';
+// import CreateTransactionService from '../services/CreateTransactionService';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactonDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface TransactionALl {
+  transaction: [];
+  balance: number;
 }
 
 class TransactionsRepository {
@@ -13,16 +25,51 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all() {
+    const all = {
+      transactions: this.transactions,
+      balance: this.getBalance(),
+    };
+    return all;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomesAll = this.transactions.filter(
+      index => index.type === 'income',
+    );
+    const outcomesAll = this.transactions.filter(
+      index => index.type === 'outcome',
+    );
+
+    const getIncomesValues = incomesAll.map(index => index.value);
+    const getOutcomesValues = outcomesAll.map(index => index.value);
+
+    const totalIncomes = getIncomesValues.reduce(
+      (total, numero) => total + numero,
+      0,
+    );
+    const totalOutcomes = getOutcomesValues.reduce(
+      (total, numero) => total + numero,
+      0,
+    );
+
+    const totalBalance = totalIncomes - totalOutcomes;
+
+    const balance = {
+      income: totalIncomes,
+      outcome: totalOutcomes,
+      total: totalBalance,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactonDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,17 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+
+    return response.json(transactions);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +19,32 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    if (type !== 'outcome' && type !== 'income') {
+      return response.status(400).json({ error: 'Type dont is correct' });
+    }
+    if (type === 'outcome') {
+      const transactionsBalance = transactionsRepository.getBalance();
+      if (value > transactionsBalance.total) {
+        return response.status(400).json({
+          error:
+            'This operation is not suported, becouse your balance is less than the amount entered',
+        });
+      }
+    }
+
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const transaction = createTransaction.execute({
+      title,
+      value,
+      type,
+    });
+
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
